08c8658914ea8d91c12d35787dead73a719c56f8
Ben Blackmore <ben.blackmore@dash0.com>
2023-07-12 14:57:15 +0200

    Revert "feat: correct prisma config"
    This reverts commit 7b01a832f4d4922925887833f731c95960716c73.

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,8 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string(),
+    DATABASE_URL: z.string().url(),
+    DATABASE_PROVIDER: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
   },
 
@@ -26,6 +27,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_PROVIDER: process.env.DATABASE_PROVIDER,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
