9c751c2903bc1f3a0ade726434fb2d6ed5f6a666
roshan.gh <roshanghojoghi@gmail.com>
2023-07-12 18:07:26 +0300

    shadcn/ui install

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,14 +1,20 @@
 import Editor from '@monaco-editor/react';
+import { Button } from './ui/button';
 
 export default function MonacoEditor() {
+
     return (
-        <Editor
-            height="100vh"
-            width={'50%'}
-            defaultLanguage="yaml"
-            defaultValue="hello world"
-            theme="vs-dark"
-            options={{ automaticLayout: true }}
-        />
+        <div className="flex gap-x-4">
+            <Editor
+                height="100vh"
+                width={'50%'}
+                defaultLanguage="yaml"
+                defaultValue="hello world"
+                theme="vs-dark"
+                options={{ automaticLayout: true }}
+            />
+            <div className='flex flex-col gap-y-4'>
+            </div>
+        </div>
     );
 }
