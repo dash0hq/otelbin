ac21d7b8f1ebcd436959c523a71b8e41f1c0023c
roshan.gh <roshanghojoghi@gmail.com>
2023-07-13 14:47:40 +0300

    eslint for ts edit (temp)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,9 @@ const path = require("path");
 const config = {
   overrides: [
     {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
+      // extends: [
+      //   "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      // ],
       files: ["*.ts", "*.tsx"],
       parserOptions: {
         project: path.join(__dirname, "tsconfig.json"),
