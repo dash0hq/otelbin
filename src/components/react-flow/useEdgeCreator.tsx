c8b7f56826de7b8ef44d2a535d24f0a1e0d95ad7
Nusha <m.akbarizadeh@orangeryplus.de>
2023-07-28 03:41:35 +0300

    one step bach into create edge

--- src/components/react-flow/useEdgeCreator.tsx
+++ src/components/react-flow/useEdgeCreator.tsx
@@ -7,59 +7,88 @@ function useEdgeCreator(nodeIdsArray: Node[]) {
   const nodeTraces = nodeIdsArray.filter((node) => node.parentNode === 'traces');
 
   const [edgeList, setEdgeList] = useState<Edge[]>([]);
+  const edgesToAdd: Edge[] = [];
+  const addToLogs = (nodeLogs: Node[]) => {
+    for (let i = 0; i < nodeLogs.length - 1; i++) {
+      const sourceNode = nodeLogs[i];
+      const targetNode = nodeLogs[i + 1];
 
+      if (!sourceNode || !targetNode) {
+        // Handle the case when source or target node is undefined or null
+        console.error('Invalid node found.');
+        continue;
+      }
+
+      const sourceNodeId = sourceNode.id;
+      const targetNodeId = targetNode.id;
+
+      const edgeId = `edge-${sourceNodeId}-${targetNodeId}`;
+
+      const edge: Edge = {
+        id: edgeId,
+        source: sourceNodeId,
+        target: targetNodeId,
+      };
+      edgesToAdd.push(edge);
+    }
+  };
+  const addToMetrics = (nodeMetrics: Node[]) => {
+    for (let i = 0; i < nodeMetrics.length - 1; i++) {
+      const sourceNode = nodeMetrics[i];
+      const targetNode = nodeMetrics[i + 1];
+
+      if (!sourceNode || !targetNode) {
+        // Handle the case when source or target node is undefined or null
+        console.error('Invalid node found.');
+        continue;
+      }
+
+      const sourceNodeId = sourceNode.id;
+      const targetNodeId = targetNode.id;
+
+      const edgeId = `edge-${sourceNodeId}-${targetNodeId}`;
+
+      const edge: Edge = {
+        id: edgeId,
+        source: sourceNodeId,
+        target: targetNodeId,
+      };
+      edgesToAdd.push(edge);
+    }
+  };
+  const addToTraces = (nodeTraces: Node[]) => {
+    for (let i = 0; i < nodeTraces.length - 1; i++) {
+      const sourceNode = nodeTraces[i];
+      const targetNode = nodeTraces[i + 1];
+
+      if (!sourceNode || !targetNode) {
+        // Handle the case when source or target node is undefined or null
+        console.error('Invalid node found.');
+        continue;
+      }
+
+      const sourceNodeId = sourceNode.id;
+      const targetNodeId = targetNode.id;
+
+      const edgeId = `edge-${sourceNodeId}-${targetNodeId}`;
+
+      const edge: Edge = {
+        id: edgeId,
+        source: sourceNodeId,
+        target: targetNodeId,
+      };
+      edgesToAdd.push(edge);
+    }
+  };
   useEffect(() => {
     if (!Array.isArray(nodeIdsArray) || nodeIdsArray.length < 2) {
       console.error('Invalid input: An array of at least two node IDs is required.');
       return;
     }
 
-    const edgesToAdd: Edge[] = [];
-
-    const addToEdges = (nodes: Node[]) => {
-      for (let i = 0; i < nodes.length - 1; i++) {
-        const sourceNode = nodes[i];
-        const targetNode = nodes[i + 1];
-
-        if (!sourceNode || !targetNode) {
-          console.error('Invalid node found.');
-          continue;
-        }
-
-        const sourceNodeId = sourceNode.id;
-        const targetNodeId = targetNode.id;
-
-        const edgeId = `edge-${sourceNodeId}-${targetNodeId}`;
-
-        const edge: Edge = {
-          id: edgeId,
-          source: sourceNodeId,
-          target: targetNodeId,
-        };
-        edgesToAdd.push(edge);
-      }
-    };
-
-    addToEdges(nodeLogs);
-    addToEdges(nodeMetrics);
-    addToEdges(nodeTraces);
-
-    if (nodeTraces.length > 0) {
-      const lastProcessorNode = nodeTraces[nodeTraces.length - 1];
-      const exporterNodes = nodeIdsArray.filter((node) => node.type === 'exporterNode');
-      
-      if (exporterNodes.length > 0) {
-        const firstExporterNode = exporterNodes[0];
-        const edgeId = `edge-${lastProcessorNode?.id}-${firstExporterNode?.id}`;
-        
-        const edge: Edge = {
-          id: edgeId,
-          source: lastProcessorNode?.id!,
-          target: firstExporterNode?.id!,
-        };
-        edgesToAdd.push(edge);
-      }
-    }
+    addToLogs(nodeLogs)
+    addToMetrics(nodeMetrics)
+    addToTraces(nodeTraces)
 
     setEdgeList(edgesToAdd);
   }, [nodeIdsArray]);
