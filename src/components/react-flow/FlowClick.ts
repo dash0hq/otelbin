386a83e5b490791bcb8f010a8d9b3100abeccd2b
roshan.gh <roshanghojoghi@gmail.com>
2023-08-03 14:58:47 +0300

    Flow click func code refactor

--- src/components/react-flow/FlowClick.ts
+++ src/components/react-flow/FlowClick.ts
@@ -1,7 +1,6 @@
 import type { editor } from "monaco-editor";
 import type { RefObject } from "react";
 
-
 type EditorRefType = RefObject<editor.IStandaloneCodeEditor | null>;
 
 interface IData {
@@ -12,133 +11,60 @@ interface IData {
 export function FlowClick(event: React.MouseEvent, data: IData, editorRef: EditorRefType | null, section: string) {
     event.stopPropagation();
 
-    const findMatch = (label: string) => (editorRef?.current?.getModel()?.findMatches(label, true, false, false, null, true));
+    const findMatch = (label: string) => editorRef?.current?.getModel()?.findMatches(label, true, false, false, null, true);
 
     const indentationRegex = /^(\s*)/;
 
-    const pipelinePositions: editor.FindMatch[] | undefined = findMatch('pipelines');
-    const pipeLinePosition = pipelinePositions && pipelinePositions.length > 0 && pipelinePositions[0] || null;
+    const getStartPosition = (keyword: string) => {
+        const positions = findMatch(keyword);
+        const startLine = positions && positions[0]?.range.startLineNumber || 0;
+        return { positions, startLine };
+    };
+
+
+    const { positions: pipelinePositions, startLine: pipeLinesStartLine } = getStartPosition('pipelines');
     const lines = editorRef?.current?.getModel()?.getValue().split(/\r?\n/) || [];
-    const afterPipeLinesIndentationCount = lines[pipeLinePosition?.range.startLineNumber || 0]?.match(indentationRegex)?.[0].length || 0;
+    const afterPipeLinesIndentationCount = lines[pipeLinesStartLine]?.match(indentationRegex)?.[0].length || 0;
 
-    const pipeLinesStartNumber = pipeLinePosition?.range.startLineNumber || 0;
-    let pipeLinesEndLine = pipeLinesStartNumber || 0;
-    for (let i = pipeLinesStartNumber || 0; i < lines.length; i++) {
-        if (lines[i]?.match(indentationRegex)?.[0].length || 0 >= afterPipeLinesIndentationCount) (pipeLinesEndLine += 1);
+    let pipeLinesEndLine = pipeLinesStartLine;
+    for (let i = pipeLinesStartLine; i < lines.length; i++) {
+        if ((lines[i]?.match(indentationRegex)?.[0].length || 0) >= afterPipeLinesIndentationCount) {
+            pipeLinesEndLine += 1;
+        }
     }
 
-    const logsPositions: editor.FindMatch[] | undefined = findMatch('logs');
-    const logsPosition = logsPositions && logsPositions.length > 0 && logsPositions.filter((position) => {
-        return position.range.startLineNumber > pipeLinesStartNumber && position.range.startLineNumber <= pipeLinesEndLine
-    });
-    const logsStartLine = logsPosition && logsPosition[0] && logsPosition[0].range.startLineNumber || 0;
+    const getStartPositionInPipeline = (keyword: string) => {
+        const positions = findMatch(keyword);
+        return positions?.filter((position) => position.range.startLineNumber > getStartPosition('pipelines').startLine && position.range.startLineNumber < pipeLinesEndLine)[0]?.range.startLineNumber;
+    };
 
-
-    const tracesPositions: editor.FindMatch[] | undefined = findMatch('traces');
-    const tracesPosition = tracesPositions && tracesPositions.length > 0 && tracesPositions.filter((position) => {
-        return position.range.startLineNumber > pipeLinesStartNumber && position.range.startLineNumber < pipeLinesEndLine
-    });
-    const tracesStartLine = tracesPosition && tracesPosition[0] && tracesPosition[0].range.startLineNumber || 0;
-
-
-    const metricsPositions: editor.FindMatch[] | undefined = findMatch('metrics');
-    const metricsPosition = metricsPositions && metricsPositions.length > 0 && metricsPositions.filter((position) => {
-        return position.range.startLineNumber > pipeLinesStartNumber && position.range.startLineNumber < pipeLinesEndLine
-    });
-    const metricsStartLine = metricsPosition && metricsPosition[0] && metricsPosition[0].range.startLineNumber || 0;
-
-    const changePosition = (position: editor.FindMatch[]) => {
-        editorRef?.current?.setPosition({ lineNumber: position[0] && position[0].range.startLineNumber || 1, column: position[0] && position[0].range.startColumn || 1 });
-        editorRef?.current?.focus();
-        editorRef?.current?.revealPositionInCenter({ lineNumber: position[0] && position[0].range.startLineNumber || 1, column: position[0] && position[0].range.startColumn || 1 });
-    }
-
-    let activePosition: editor.FindMatch[] | undefined = [];
-    let activePositionStartLine = 0;
-    let labelPosition: editor.FindMatch[] | undefined = []
+    const goToSection = (keyword: string, startLine: number) => {
+        const activePosition = findMatch(keyword)?.filter((position) => position.range.startLineNumber > startLine);
+        const labelPosition = findMatch(data.label)?.filter((position) => position.range.startLineNumber > (activePosition && activePosition[0]?.range.startLineNumber || 0));
+        changePosition(labelPosition || []);
+    };
 
     switch (data.parentNode) {
         case 'logs':
-            switch (section) {
-                case 'receivers':
-                    activePosition = findMatch('receivers')?.filter((position) => {
-                        return position.range.startLineNumber > logsStartLine
-                    })
-
-                    break;
-                case 'processors':
-                    activePosition = findMatch('processors')?.filter((position) => {
-                        return position.range.startLineNumber > logsStartLine
-                    })
-                    break;
-                case 'exporters':
-                    activePosition = findMatch('exporters')?.filter((position) => {
-                        return position.range.startLineNumber > logsStartLine
-                    })
-                    break;
-            }
-            activePositionStartLine = activePosition && activePosition[0] && activePosition[0].range.startLineNumber || 0;
-            labelPosition = findMatch(data.label)?.filter((position) => {
-                return position.range.startLineNumber > activePositionStartLine || 0
-            })
-            changePosition(labelPosition || []);
+            goToSection(section, getStartPositionInPipeline('logs') || 0);
             break;
 
         case 'metrics':
-            switch (section) {
-                case 'receivers':
-                    activePosition = findMatch('receivers')?.filter((position) => {
-                        return position.range.startLineNumber > metricsStartLine
-                    })
-
-                    break;
-                case 'processors':
-                    activePosition = findMatch('processors')?.filter((position) => {
-                        return position.range.startLineNumber > metricsStartLine
-                    })
-                    break;
-                case 'exporters':
-                    activePosition = findMatch('exporters')?.filter((position) => {
-                        return position.range.startLineNumber > metricsStartLine
-                    })
-                    break;
-            }
-            activePositionStartLine = activePosition && activePosition[0] && activePosition[0].range.startLineNumber || 0;
-            labelPosition = findMatch(data.label)?.filter((position) => {
-                return position.range.startLineNumber > activePositionStartLine || 0
-            })
-            changePosition(labelPosition || []);
+            goToSection(section, getStartPositionInPipeline('metrics') || 0);
             break;
 
         case 'traces':
-            switch (section) {
-                case 'receivers':
-                    activePosition = findMatch('receivers')?.filter((position) => {
-                        return position.range.startLineNumber > tracesStartLine
-                    })
-
-                    break;
-                case 'processors':
-                    activePosition = findMatch('processors')?.filter((position) => {
-                        return position.range.startLineNumber > tracesStartLine
-                    })
-                    break;
-                case 'exporters':
-                    activePosition = findMatch('exporters')?.filter((position) => {
-                        return position.range.startLineNumber > tracesStartLine
-                    })
-                    break;
-            }
-            activePositionStartLine = activePosition && activePosition[0] && activePosition[0].range.startLineNumber || 0;
-            labelPosition = findMatch(data.label)?.filter((position) => {
-                return position.range.startLineNumber > activePositionStartLine || 0
-            })
-            changePosition(labelPosition || []);
+            goToSection(section, getStartPositionInPipeline('traces') || 0);
             break;
 
         default:
             changePosition(pipelinePositions || []);
             break;
     }
-}
 
+    function changePosition(position: editor.FindMatch[]) {
+        editorRef?.current?.setPosition({ lineNumber: position[0]?.range.startLineNumber || 1, column: position[0]?.range.startColumn || 1 });
+        editorRef?.current?.focus();
+        editorRef?.current?.revealPositionInCenter({ lineNumber: position[0]?.range.startLineNumber || 1, column: position[0]?.range.startColumn || 1 });
+    }
+}
