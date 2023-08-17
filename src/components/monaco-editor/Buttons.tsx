9e62e1f089a2a33dcb81495d74e83caf051a20f0
Ben Blackmore <ben.blackmore@dash0.com>
2023-08-17 14:45:52 +0200

    feat: allow component reuse

--- src/components/monaco-editor/Buttons.tsx
+++ src/components/monaco-editor/Buttons.tsx
@@ -1,109 +1,112 @@
-import { MutableRefObject } from "react";
-import DeleteConfigButton from "./DeleteConfigButton"
-import { Button } from "../ui/button"
+import { type MutableRefObject } from "react";
+import DeleteConfigButton from "./DeleteConfigButton";
+import { Button } from "@dash0/components/ui/button";
 
-export default function Buttons(
-
-    { editorRef, monacoRef }:
-        { editorRef: MutableRefObject<any>, monacoRef: MutableRefObject<any> }) {
-
-    function handleCopy() {
-
-        const selection = editorRef.current.getSelection();
-        if (selection.isEmpty()) {
-            const allText = editorRef.current.getValue();
-            navigator.clipboard.writeText(allText);
-        } else {
-            const selectedText = editorRef.current.getModel().getValueInRange(selection);
-            navigator.clipboard.writeText(selectedText);
-        }
+export default function Buttons({
+  editorRef,
+  monacoRef,
+}: {
+  editorRef: MutableRefObject<any>;
+  monacoRef: MutableRefObject<any>;
+}) {
+  function handleCopy() {
+    const selection = editorRef.current.getSelection();
+    if (selection.isEmpty()) {
+      const allText = editorRef.current.getValue();
+      navigator.clipboard.writeText(allText);
+    } else {
+      const selectedText = editorRef.current
+        .getModel()
+        .getValueInRange(selection);
+      navigator.clipboard.writeText(selectedText);
     }
+  }
 
-    function handleDownload() {
+  function handleDownload() {
+    const allText = editorRef.current.getValue();
 
-        const allText = editorRef.current.getValue();
-
-        if (allText.length > 0) {
-            const blob = new Blob([allText], { type: 'text/plain' });
-            const url = URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.download = 'config.yaml';
-            link.href = url;
-            link.click();
-        } else {
-            alert('No text to save');
-        }
+    if (allText.length > 0) {
+      const blob = new Blob([allText], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.download = "config.yaml";
+      link.href = url;
+      link.click();
+    } else {
+      alert("No text to save");
     }
+  }
 
-    // function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
-    //     setData({ name: e.target.value, config: data.config })
-    // }
+  // function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+  //     setData({ name: e.target.value, config: data.config })
+  // }
 
-    // const submitData = async (e: React.FormEvent) => {
-    //     e.preventDefault()
-    //     if (data.name === '' || data.config === '') {
-    //         alert('Please fill all fields')
-    //         return
-    //     }
-    //     try {
-    //         const result = await mutation.mutateAsync(data);
-    //         setData({ name: '', config: '' })
-    //     } catch (err: unknown) {
+  // const submitData = async (e: React.FormEvent) => {
+  //     e.preventDefault()
+  //     if (data.name === '' || data.config === '') {
+  //         alert('Please fill all fields')
+  //         return
+  //     }
+  //     try {
+  //         const result = await mutation.mutateAsync(data);
+  //         setData({ name: '', config: '' })
+  //     } catch (err: unknown) {
 
-    //     }
-    //     setClicked(false)
-    // }
-    return (<></>
-        // <div className='flex w-full gap-x-4'>
-        //     <div className='flex flex-col gap-y-4 h-[100vh]'>
-        //         <div className='flex flex-col gap-y-4 w-56'>
-        //             <Button
-        //                 onClick={handleCopy}>
-        //                 Copy
-        //             </Button>
-        //             <Button
-        //                 onClick={handleDownload}>
-        //                 Download
-        //             </Button>
-        //             {!clicked && <Button
-        //                 onClick={() => {
-        //                     setData({ name: '', config: '' })
-        //                     setClicked(true)
-        //                 }}
-        //             >
-        //                 Create New
-        //             </Button>}
+  //     }
+  //     setClicked(false)
+  // }
+  return (
+    <></>
+    // <div className='flex w-full gap-x-4'>
+    //     <div className='flex flex-col gap-y-4 h-[100vh]'>
+    //         <div className='flex flex-col gap-y-4 w-56'>
+    //             <Button
+    //                 onClick={handleCopy}>
+    //                 Copy
+    //             </Button>
+    //             <Button
+    //                 onClick={handleDownload}>
+    //                 Download
+    //             </Button>
+    //             {!clicked && <Button
+    //                 onClick={() => {
+    //                     setData({ name: '', config: '' })
+    //                     setClicked(true)
+    //                 }}
+    //             >
+    //                 Create New
+    //             </Button>}
 
-        //             {clicked && <div className='flex gap-x-4'>
-        //                 <Input
-        //                     value={data.name}
-        //                     onChange={handleChangeInput}
-        //                     placeholder="config name"
-        //                 />
-        //                 <Button onClick={submitData}>
-        //                     Submit
-        //                 </Button>
-        //             </div>}
-        //         </div>
-        //         <div className='flex flex-col max-h-[400px] overflow-y-auto'>
-        //             {configs && configs?.length > 0 && configs.map((config) => {
-        //                 return (
-        //                     <div className='flex' key={config.id}>
-        //                         <Button className='min-w-[250px]'
-        //                             onClick={() => {
-        //                                 setClicked(false)
-        //                                 Router.push(`/config/${config.id}`)
-        //                                 handleYamlValidation(config.config)
-        //                             }}
-        //                             variant={'outline'}>
-        //                             {config.name}
-        //                         </Button>
-        //                         <DeleteConfigButton config={config} />
-        //                     </div>
-        //                 )
-        //             })}
-        //         </div>
-        //     </div>
-        // </div>
-    )
+    //             {clicked && <div className='flex gap-x-4'>
+    //                 <Input
+    //                     value={data.name}
+    //                     onChange={handleChangeInput}
+    //                     placeholder="config name"
+    //                 />
+    //                 <Button onClick={submitData}>
+    //                     Submit
+    //                 </Button>
+    //             </div>}
+    //         </div>
+    //         <div className='flex flex-col max-h-[400px] overflow-y-auto'>
+    //             {configs && configs?.length > 0 && configs.map((config) => {
+    //                 return (
+    //                     <div className='flex' key={config.id}>
+    //                         <Button className='min-w-[250px]'
+    //                             onClick={() => {
+    //                                 setClicked(false)
+    //                                 Router.push(`/config/${config.id}`)
+    //                                 handleYamlValidation(config.config)
+    //                             }}
+    //                             variant={'outline'}>
+    //                             {config.name}
+    //                         </Button>
+    //                         <DeleteConfigButton config={config} />
+    //                     </div>
+    //                 )
+    //             })}
+    //         </div>
+    //     </div>
+    // </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ossTailwindConfig = require("@dash0hq/ui/tailwind.config");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  presets: [require("./src/design-tokens/tailwind.preset.js")],
+  ...ossTailwindConfig,
+
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    // for dash0-admin
+    ...ossTailwindConfig.content,
+    // for dash0-oss
+    ...ossTailwindConfig.content.map((p) =>
+      path.join(__dirname, "node_modules", "@dash0hq/ui", p)
+    ),
   ],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        blackGrey: "#030816",
-        lightGrey1: "#8491A6",
-        primaryMagenta: "#C93A76",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
 };
