b605f7e01b5d8cf306ef6127551e0be47df30d22
roshan.gh <roshanghojoghi@gmail.com>
2023-08-20 18:24:33 +0300

    share button

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,6 +3,7 @@ import { ButtonGroup } from "@dash0/components/ui/button-group";
 import { IconButton } from '@dash0hq/ui/src/components/ui/icon-button';
 import { Columns, Code2, Share2 } from 'lucide-react';
 import { ServiceMapIcon } from "@dash0/icons";
+import { useToast } from '@dash0hq/ui/src/components/ui/use-toast';
 
 
 export default function AppHeader(
@@ -17,6 +18,15 @@ export default function AppHeader(
     const handleViewChange = (view: string) => {
         setView(view);
     };
+
+    const { toast } = useToast();
+
+    function handleShare() {
+        navigator.clipboard.writeText(window.location.href);
+        toast({
+            description: "Current config copied to the clipboard.",
+        })
+    }
 
     return (
         <div className='h-[48px] bg-[#0F172A] flex justify-between items-center px-4 py-2  border-b-1'>
@@ -44,7 +54,7 @@ export default function AppHeader(
             </ButtonGroup>
                 <IconButton
                     className={`${activeView === 'code' && 'bg-[#6D737D]'} min-w-[93px] bg-[#6366F1]`}
-                    onClick={() => handleViewChange('code')}
+                    onClick={handleShare}
                     variant={'default'} size={'sm'} >
 
                     <Share2 color='white' className='mr-2' />
