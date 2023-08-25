792b6c865abd15aab6beb629fd6be24502b7164b
roshan.gh <roshanghojoghi@gmail.com>
2023-08-25 15:02:58 +0300

    width handler hover effect, format MonacoEditor file

--- src/components/monaco-editor/MonacoEditor.tsx
+++ src/components/monaco-editor/MonacoEditor.tsx
@@ -1,41 +1,49 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import React from 'react';
-import { useConfigs, useInsertConfigs } from '~/queries/config';
-import type { IAjvError, IError } from './ErrorConsole';
-import { schema } from './JSONSchema';
-import ErrorConsole from './ErrorConsole';
-import EditorTopBar from '../EditorTopBar';
-import { DefaultConfig } from './DefaultConfig';
-import { useEditorRef, useEditorDidMount, useMonacoRef } from '~/contexts/EditorContext';
-import Editor from '@monaco-editor/react';
-import JsYaml from 'js-yaml';
-import Ajv from "ajv"
-import { ReactFlowProvider } from 'reactflow';
-import Flow from '../react-flow/ReactFlow';
-import { useMouseDelta } from './MouseDelta';
-import { useRouter } from 'next/router';
-import { useUrlState } from '~/lib/urlState/client/useUrlState';
-import AppHeader from '../AppHeader';
-import WelcomeModal from '../welcome-modal/WelcomeModal';
-
+import { useCallback, useEffect, useRef, useState } from "react";
+import React from "react";
+import { useConfigs, useInsertConfigs } from "~/queries/config";
+import type { IAjvError, IError } from "./ErrorConsole";
+import { schema } from "./JSONSchema";
+import ErrorConsole from "./ErrorConsole";
+import EditorTopBar from "../EditorTopBar";
+import { DefaultConfig } from "./DefaultConfig";
+import {
+    useEditorRef,
+    useEditorDidMount,
+    useMonacoRef,
+} from "~/contexts/EditorContext";
+import Editor from "@monaco-editor/react";
+import JsYaml from "js-yaml";
+import Ajv from "ajv";
+import { ReactFlowProvider } from "reactflow";
+import Flow from "../react-flow/ReactFlow";
+import { useMouseDelta } from "./MouseDelta";
+import { useRouter } from "next/router";
+import { useUrlState } from "~/lib/urlState/client/useUrlState";
+import AppHeader from "../AppHeader";
+import WelcomeModal from "../welcome-modal/WelcomeModal";
 
 export default function MonacoEditor({ id }: { id?: string }) {
     const editorDidMount = useEditorDidMount();
     const editorRef = useEditorRef();
     const monacoRef = useMonacoRef();
-    const [clicked, setClicked] = useState(false)
-    const [data, setData] = useState({ name: '', config: '' })
+    const [clicked, setClicked] = useState(false);
+    const [data, setData] = useState({ name: "", config: "" });
     const [errors, setErrors] = useState<IError>({});
-    const { data: configs } = useConfigs()
-    const mutation = useInsertConfigs()
+    const { data: configs } = useConfigs();
+    const mutation = useInsertConfigs();
     const editorDivRef = useRef(null);
-    const savedWidth = typeof window !== "undefined" ? localStorage.getItem('width') : '';
+    const savedWidth =
+        typeof window !== "undefined" ? localStorage.getItem("width") : "";
     const width = useMouseDelta(Number(savedWidth) || 440, editorDivRef);
-    const [isServer, setIsServer] = useState<boolean>(false)
+    const [isServer, setIsServer] = useState<boolean>(false);
     const router = useRouter();
     const [activeView, setActiveView] = useState("both");
-    const savedOpenModal = Boolean(typeof window !== "undefined" && localStorage.getItem('welcomeModal'));
-    const [openDialog, setOpenDialog] = useState(savedOpenModal ? !savedOpenModal : true);
+    const savedOpenModal = Boolean(
+        typeof window !== "undefined" && localStorage.getItem("welcomeModal")
+    );
+    const [openDialog, setOpenDialog] = useState(
+        savedOpenModal ? !savedOpenModal : true
+    );
 
     const editorBinding = {
         prefix: "",
@@ -53,28 +61,26 @@ export default function MonacoEditor({ id }: { id?: string }) {
     );
 
     useEffect(() => {
-        setIsServer(true)
-    }, [])
+        setIsServer(true);
+    }, []);
 
     function handleYamlValidation(configData: string) {
-        const ajv = new Ajv({ allErrors: true })
-        const model = editorRef && editorRef.current && editorRef.current.getModel();
-        let ajvError: IAjvError[] = []
+        const ajv = new Ajv({ allErrors: true });
+        const model =
+            editorRef && editorRef.current && editorRef.current.getModel();
+        let ajvError: IAjvError[] = [];
         try {
             const jsonData = JsYaml.load(configData);
             const valid = ajv.validate(schema, jsonData);
             if (!valid) {
-
                 const errors = ajv.errors;
 
                 if (errors) {
-
                     const validationErrors = errors.map((error: any) => {
-
                         const errorInfo = {
                             line: null as number | null,
                             column: null as number | null,
-                            message: error.message || 'Unknown error',
+                            message: error.message || "Unknown error",
                         };
 
                         if (error instanceof JsYaml.YAMLException) {
@@ -83,14 +89,13 @@ export default function MonacoEditor({ id }: { id?: string }) {
                         }
 
                         return errorInfo;
-
                     });
-                    ajvError.push(...validationErrors)
+                    ajvError.push(...validationErrors);
                 }
             } else {
-                ajvError = []
+                ajvError = [];
             }
-            setErrors({ ajvErrors: ajvError })
+            setErrors({ ajvErrors: ajvError });
             if (model) {
                 monacoRef.current?.editor.setModelMarkers(model, "json", []);
             }
@@ -110,70 +115,89 @@ export default function MonacoEditor({ id }: { id?: string }) {
             if (model) {
                 monacoRef.current?.editor.setModelMarkers(model, "json", [errorMarker]);
             }
-            setErrors({ jsYamlError: error })
+            setErrors({ jsYamlError: error });
         }
     }
 
     return (
         <>
-            {isServer
-                ? <WelcomeModal open={openDialog} setOpen={setOpenDialog} />
-                : <></>}
-            <div className="flex flex-col h-full">
+            {isServer ? (
+                <WelcomeModal open={openDialog} setOpen={setOpenDialog} />
+            ) : (
+                <></>
+            )}
+            <div className="flex h-full flex-col">
                 <AppHeader activeView={activeView} setView={setActiveView} />
-            <div className="flex">
-            {isServer
-                ? <div ref={editorDivRef}
-                    style={{
-                        position: 'relative',
-                        width: activeView === 'both' ? `${width}px` : activeView === 'code' ? '100%' : '0px',
-                        backgroundColor: '#1E1E1E',
-                        borderRight: '8px solid #40454E',
-                        cursor: activeView === 'both' ? 'col-resize' : 'default',
-                        userSelect: 'none',
-                        display: 'flex',
-                        flexDirection: 'column'
-                            }}>
-                        <EditorTopBar />
-                <Editor
-
-                        defaultValue={config.length ? config : DefaultConfig}
-                    value={
-                        !clicked ?
-                            configs && configs?.length > 0 &&
-                            configs.filter((config) => config.id?.toString() === id)[0]?.config || data.config
-                            : data.config
-                    }
-                    onMount={editorDidMount}
-                            height="94.5vh"
-                                width={'100%'}
-                    defaultLanguage='yaml'
-                    theme="vs-dark"
-                                options={{ automaticLayout: true, minimap: { enabled: false }, scrollbar: { verticalScrollbarSize: 5 }, padding: { top: 10 } }}
-                    onChange={
-                        (value) => {
-                            setData({
-                                name: data.name,
-                                config: value || ''
-                            })
-                            onChangeConfig(value || '')
-                            handleYamlValidation(value || '')
-                        }
-                    }
-                />
-                <ErrorConsole errors={errors} />
+                <div className="flex">
+                    {isServer ? (
+                        <div
+                            ref={editorDivRef}
+                            className={`relative flex select-none flex-col border-r-[8px] border-otelbinDarkBlue2 hover:border-otelbinDarkBlue3`}
+                            style={{
+                                width:
+                                    activeView === "both"
+                                        ? `${width}px`
+                                        : activeView === "code"
+                                            ? "100%"
+                                            : "0px",
+                                cursor: activeView === "both" ? "col-resize" : "default",
+                            }}
+                        >
+                            <EditorTopBar />
+                            <Editor
+                                defaultValue={config.length ? config : DefaultConfig}
+                                value={
+                                    !clicked
+                                        ? (configs &&
+                                            configs?.length > 0 &&
+                                            configs.filter(
+                                                (config) => config.id?.toString() === id
+                                            )[0]?.config) ||
+                                        data.config
+                                        : data.config
+                                }
+                                onMount={editorDidMount}
+                                height="94.5vh"
+                                width={"100%"}
+                                defaultLanguage="yaml"
+                                theme="vs-dark"
+                                options={{
+                                    automaticLayout: true,
+                                    minimap: { enabled: false },
+                                    scrollbar: { verticalScrollbarSize: 5 },
+                                    padding: { top: 10 },
+                                }}
+                                onChange={(value) => {
+                                    setData({
+                                        name: data.name,
+                                        config: value || "",
+                                    });
+                                    onChangeConfig(value || "");
+                                    handleYamlValidation(value || "");
+                                }}
+                            />
+                            <ErrorConsole errors={errors} />
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                    <div className="z-0 flex-grow-[3]" style={{ height: "94.5vh" }}>
+                        <ReactFlowProvider>
+                            <Flow
+                                value={
+                                    (errors?.jsYamlError === undefined &&
+                                        errors.ajvErrors?.length === 0 &&
+                                        configs &&
+                                        configs?.length > 0 &&
+                                        config) ||
+                                    config ||
+                                    DefaultConfig
+                                }
+                            />
+                        </ReactFlowProvider>
+                    </div>
+                </div>
             </div>
-                : <></>}
-                <div className='z-0 flex-grow-[3]' style={{ height: '94.5vh' }}>
-                <ReactFlowProvider>
-                    <Flow value={(errors?.jsYamlError === undefined && errors.ajvErrors?.length === 0
-                        && configs && configs?.length > 0 && config || config) || DefaultConfig
-                    }
-                    />
-                </ReactFlowProvider>
-            </div>
-        </div>
-        </div>
         </>
     );
 }
