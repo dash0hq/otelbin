a7611a7ccb14355e1bec26304b69429cf3772fbe
Ben Blackmore <ben.blackmore@dash0.com>
2023-09-07 15:34:23 +0200

    feat: smooth color transition in footer

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -11,7 +11,7 @@ export function AppFooter() {
 		<div className="flex shrink-0 items-center justify-between border-t-1 border-subtle bg-neutral-150 px-4 py-2 text-xs font-normal text-subtl">
 			<div className="flex gap-4">
 				<a
-					className="flex cursor-pointer items-center gap-1 hover:text-default"
+					className="flex cursor-pointer items-center gap-1 transition-colors hover:text-default"
 					href="https://join.slack.com/t/dash0-community/shared_invite/zt-21gxq6bya-VcO45Wr~nlxc37aLWqqrjA"
 					target="_blank"
 					rel="noreferrer noopener"
@@ -20,7 +20,7 @@ export function AppFooter() {
 					Community Slack
 				</a>
 				<a
-					className="flex cursor-pointer items-center gap-1 hover:text-default"
+					className="flex cursor-pointer items-center gap-1 transition-colors hover:text-default"
 					href="https://github.com/dash0hq/otelbin"
 					target="_blank"
 					rel="noreferrer noopener"
@@ -32,7 +32,7 @@ export function AppFooter() {
 			<a
 				href="https://www.dash0.com/?utm_source=otelbin&utm_medium=footer&utm_campaign=otelbin"
 				target="_blank"
-				className="flex cursor-pointer items-center whitespace-nowrap hover:text-default"
+				className="flex cursor-pointer items-center whitespace-nowrap transition-colors hover:text-default"
 			>
 				Crafted with <Love className="mx-1" /> by
 				<Dash0 fill="#6D737D" width="55px" className="ml-2" />
