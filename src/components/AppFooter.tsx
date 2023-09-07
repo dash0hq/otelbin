c25fd6e28e06ae93327b4f92cb1f1cdf553b9f4c
Ben Blackmore <ben.blackmore@dash0.com>
2023-09-07 13:07:08 +0200

    feat: finish footer

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,19 +1,40 @@
 // SPDX-FileCopyrightText: 2023 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import Love from "~/components/assets/svg/love.svg";
 import Dash0 from "~/components/assets/svg/dash0.svg";
+import { Github, Slack } from "lucide-react";
+import React from "react";
 
 export function AppFooter() {
 	return (
-		<div className="flex shrink-0 items-center justify-between border-t-1 border-subtle bg-neutral-150 px-4 py-2">
-			{/* unused left-side */}
-			<div />
+		<div className="flex shrink-0 items-center justify-between border-t-1 border-subtle bg-neutral-150 px-4 py-2 text-xs font-normal text-default">
+			<div className="flex gap-4">
+				<a
+					className="flex cursor-pointer items-center gap-1"
+					href="https://join.slack.com/t/dash0-community/shared_invite/zt-21gxq6bya-VcO45Wr~nlxc37aLWqqrjA"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<Slack size="0.875rem" />
+					Community Slack
+				</a>
+				<a
+					className="flex cursor-pointer items-center gap-1"
+					href="https://github.com/dash0hq/otelbin"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<Github size="0.875rem" />
+					GitHub
+				</a>
+			</div>
 			<a
 				href="https://www.dash0.com/?utm_source=otelbin&utm_medium=footer&utm_campaign=otelbin"
 				target="_blank"
-				className="flex whitespace-nowrap text-xs font-normal text-default"
+				className="flex cursor-pointer items-center whitespace-nowrap"
 			>
-				Crafted by
+				Crafted with <Love className="mx-1" /> by
 				<Dash0 fill="#6D737D" width="55px" className="ml-2" />
 			</a>
 		</div>
