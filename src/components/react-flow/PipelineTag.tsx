df6d5271e0a69644bcd1bc5e9ea1d862c3d48ae4
roshan.gh <roshanghojoghi@gmail.com>
2023-09-08 03:02:04 +0300

    feat: parents nodes ui update, style code refactor

--- src/components/react-flow/PipelineTag.tsx
+++ src/components/react-flow/PipelineTag.tsx
@@ -1,34 +1,43 @@
 // SPDX-FileCopyrightText: 2023 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-const calculateColor = (index: number): string => {
-	switch (index) {
-		case 0:
-			return "#F59E0B";
-		case 1:
-			return "#0AA8FF";
-		case 2:
-			return "#40ad54";
-		case 3:
-			return "#911dc9";
-	}
-	return "#FFC542";
-};
+import { BarChart4, ListTree, Workflow } from "lucide-react";
+
+const parentNodeStyle = [
+	{
+		backgroundColor: "#FBBF24",
+		icon: <Workflow width={12} />,
+	},
+	{
+		backgroundColor: "#38BDF8",
+		icon: <BarChart4 width={12} />,
+	},
+	{
+		backgroundColor: "#34D399",
+		icon: <ListTree width={12} />,
+	},
+	{
+		backgroundColor: "#911dc9",
+		icon: <Workflow width={12} />,
+	},
+];
 
 export default function PipelineTag({ findIndex, tag }: { findIndex: number; tag: string }) {
-	const capitalLabel = tag.toUpperCase();
-	const tagColor = {
-		backgroundColor: calculateColor(findIndex),
-		color: "#fff",
-		fontSize: "12px",
-		fontWeight: 500,
-		borderRadius: "5px",
-		width: "fit-content",
-		padding: "2px 5px",
-	};
 	return (
 		<>
-			<div style={tagColor}>{capitalLabel}</div>
+			{parentNodeStyle
+				.filter((_, idx) => idx === findIndex)
+				.map((node) => {
+					return (
+						<div
+							style={{ backgroundColor: node.backgroundColor, borderRadius: "4px 0px 4px 0px" }}
+							className="flex w-fit items-center gap-x-1 px-2 py-1 text-xs font-semibold text-black"
+						>
+							{node.icon}
+							<p>{tag.charAt(0).toUpperCase() + tag.slice(1)}</p>
+						</div>
+					);
+				})}
 		</>
 	);
 }
