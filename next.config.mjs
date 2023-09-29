34d26f81b093959e48031a22e8fa14bc7d01f1d2
Ben Blackmore <ben.blackmore@dash0.com>
2023-09-29 14:47:59 +0200

    feat: encourage scrolling/zooming on mobile devices to retain basic usage of OTelBin on such devices

--- next.config.mjs
+++ next.config.mjs
@@ -25,37 +25,37 @@ const config = {
 	async headers() {
 		const headers = [
 			{
-				key: 'X-Frame-Options',
-				value: 'DENY',
+				key: "X-Frame-Options",
+				value: "DENY",
 			},
 			{
-				key: 'X-Content-Type-Options',
-				value: 'nosniff',
+				key: "X-Content-Type-Options",
+				value: "nosniff",
 			},
 			{
-				key: 'Referrer-Policy',
-				value: 'origin-when-cross-origin',
+				key: "Referrer-Policy",
+				value: "origin-when-cross-origin",
 			},
 			{
-				key: 'X-Permitted-Cross-Domain-Policies',
-				value : 'none'
-			}
+				key: "X-Permitted-Cross-Domain-Policies",
+				value: "none",
+			},
 		];
 
-		if (process.env.NODE_ENV === 'production') {
+		if (process.env.NODE_ENV === "production") {
 			headers.push({
-				key: 'Strict-Transport-Security',
-				value: 'max-age=31536000; includeSubDomains'
-			})
+				key: "Strict-Transport-Security",
+				value: "max-age=31536000; includeSubDomains",
+			});
 		}
 
 		return [
 			{
-				source: '/(.*)',
+				source: "/(.*)",
 				headers,
 			},
 		];
-	}
+	},
 };
 
 export default config;
