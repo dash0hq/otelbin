487c74e5df11b0e3c4cbf391d8419c5f5a39ed2f
Ben Blackmore <ben.blackmore@dash0.com>
2023-09-29 10:47:04 +0200

    fix: revise error dialog

--- src/components/react-flow/node-types/EmptyStateNode.tsx
+++ src/components/react-flow/node-types/EmptyStateNode.tsx
@@ -8,26 +8,30 @@ import { type Node } from "reactflow";
 
 const EmptyStateNode = () => {
 	return (
-		<div className="flex flex-col bg-transparent text-default max-w-[487px] items-center gap-y-2">
-			<p className="text-[18px] font-medium">Something went wrong</p>
+		<div className="flex flex-col bg-default border-subtle border-1 text-default max-w-[487px] items-center gap-y-2 py-3 px-3 rounded-md">
+			<p className="text-[18px] font-medium">Syntax Errors in Configuration</p>
 			<p className="text-[14px] font-normal text-center">
-				You can get support from the exceptionally smart developers in our open community.
+				Try fixing the YAML syntax errors, or get help through{" "}
+				<code className="whitespace-nowrap bg-neutral-150 px-1 py-0.5 rounded-sm">#otel-collector</code> on the CNCF
+				Slack.
 			</p>
-			<div className="flex items-center justify-center gap-x-2 mt-4">
-				<a
-					href="https://join.slack.com/t/dash0-community/shared_invite/zt-21gxq6bya-VcO45Wr~nlxc37aLWqqrjA"
-					target="_blank"
-					rel="noreferrer noopener"
-				>
+			<div className="flex items-center justify-center gap-2 mt-1 flex-wrap">
+				<a href="https://slack.cncf.io/" target="_blank" rel="noreferrer noopener">
 					<Button className="text-[13px] font-medium text-default">
 						<Slack width={16} />
-						Discuss on Slack
+						Join the CNCF Slack
 					</Button>
 				</a>
-				<a href="https://github.com/dash0hq/otelbin" target="_blank" rel="noreferrer noopener">
+				<a href="https://slack.cncf.io/" target="_blank" rel="noreferrer noopener">
+					<Button className="text-[13px] font-medium text-default">
+						<Slack width={16} />
+						Join the Dash0 Slack
+					</Button>
+				</a>
+				<a href="https://github.com/dash0hq/otelbin/issues/new" target="_blank" rel="noreferrer noopener">
 					<Button className="text-[13px] font-medium text-default">
 						<Github width={16} />
-						Follow on GitHub
+						Report an OTelBin issue
 					</Button>
 				</a>
 			</div>
