1be67535d41631412a4b21d5e75c1ce8ea0e94de
Ben Blackmore <ben.blackmore@dash0.com>
2023-10-06 15:46:34 +0200

    feat: proxy the backend API

---
+++ src/app/validation/supported-distribtions/route.ts
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2023 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { NextResponse } from "next/server";
+import { assertValue } from "~/lib/env";
+
+export async function GET(): Promise<NextResponse> {
+	const response = await fetch(
+		`${assertValue(
+			process.env.COLLECTOR_CONFIGURATION_VALIDATION_URL,
+			"COLLECTOR_CONFIGURATION_VALIDATION_URL env var is not configured"
+		)}/validation/supported-distributions`,
+		{
+			headers: {
+				"X-API-KEY": assertValue(
+					process.env.COLLECTOR_CONFIGURATION_VALIDATION_API_KEY,
+					"COLLECTOR_CONFIGURATION_VALIDATION_API_KEY env var is not configured"
+				),
+			},
+		}
+	);
+
+	if (!response.ok) {
+		const body = await response.text();
+		return NextResponse.json(
+			{
+				error: body,
+			},
+			{ status: response.status }
+		);
+	}
+
+	const body = await response.json();
+	return NextResponse.json(body, {
+		headers: {
+			"Cache-Control": "max-age=120, stale-while-revalidate=600",
+		},
+	});
+}
---
+++ src/app/validation/types.ts
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2023 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export interface Distributions {
+	[id: string]: Distribution;
+}
+
+export interface Distribution {
+	provider: string;
+	description: string;
+	website: string;
+	repository: string;
+	releases: Release[];
+}
+
+export interface Release {
+	version: string;
+	artifact: string;
+}
