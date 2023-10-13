5042de5c1a954e3fe52a56424c7d3fc791d0594d
Michele Mancioppi <michele.mancioppi@dash0.com>
2023-10-13 12:59:21 +0200

    chore: unify Jest config to the typescript one

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from "jest";
 
 const config: Config = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	transform: {
+		"^.+\\.ts?$": "ts-jest",
+	},
+	transformIgnorePatterns: ["<rootDir>/node_modules/"],
 	testPathIgnorePatterns: ["validation-src/*"],
 };
 
