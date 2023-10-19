b54972ec900cb5b926624fac77164e9d1cebaae3
Ben Blackmore <ben.blackmore@dash0.com>
2023-10-19 16:19:14 +0200

    feat: add mocked validation button

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,10 @@
 // SPDX-FileCopyrightText: 2023 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import Down from "./assets/svg/down.svg";
 import Logo from "./assets/svg/otelbin_logo_white.svg";
 import { ButtonGroup, ButtonGroupItem } from "~/components/button-group";
-import { Columns, Code2, LogIn } from "lucide-react";
+import { Columns, Code2, LogIn, ChevronDown } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/tooltip";
 import { Share } from "~/components/share/Share";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
@@ -34,9 +35,17 @@ export default function AppHeader({ activeView }: { activeView: string }) {
 
 	return (
 		<div className="flex shrink-0 items-center justify-between border-b-1 border-subtle bg-neutral-150 px-4 py-3">
-			<div className="flex items-center gap-x-2">
-				<Logo height={22} />
-				<p className="text-md font-bold text-neutral-950">OTelBin</p>
+			<div className="flex items-center gap-x-4">
+				<div className="flex items-center gap-x-2">
+					<Logo height={22} />
+					<p className="text-md font-bold text-neutral-950">OTelBin</p>
+				</div>
+
+				<div className="w-px bg-neutral-350 h-full">&nbsp;</div>
+
+				<Button size="xs" variant="cta">
+					Validation: <strong>OTel Collector Contrib – v0.87.0</strong> <Down />
+				</Button>
 			</div>
 			<div className="flex gap-x-2">
 				<ButtonGroup size="xs" variant="default" className="!gap-0 bg-button">
