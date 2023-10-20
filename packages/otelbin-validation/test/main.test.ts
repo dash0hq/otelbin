21db6ed53834aa456d4cd845e7a6f44dc7bfccbd
Michele Mancioppi <michele.mancioppi@dash0.com>
2023-10-20 13:08:30 +0200

    fix: restore tests

--- packages/otelbin-validation/test/main.test.ts
+++ packages/otelbin-validation/test/main.test.ts
@@ -4,7 +4,8 @@
 
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import { describe, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
+import axios from 'axios';
 import { Distributions } from '../src/main';
 
 const apiUrl = process.env.API_GATEWAY_URL?.replace(/[\/\\]+$/, '');
@@ -42,8 +43,104 @@ const enumerateTestCases = () => {
   return testCases;
 };
 
-describe.each(enumerateTestCases())('Validation API', () => {
-  test('yes', () => {
+const assetFolderPath = join(__dirname, 'assets');
+
+describe.each(enumerateTestCases())('Validation API', (distributionName, release) => {
+
+  const validationReleasePath = `validation/${distributionName}/${release}`;
+  const validationUrl = `${apiUrl}/${validationReleasePath}`;
+
+  const otelcolConfigValid = readFileSync(join(assetFolderPath, 'config-default.yaml')).toString();
+
+  const otelcolConfigInvalidNoReceivers = readFileSync(join(assetFolderPath, 'config-no-receivers.yaml')).toString();
+
+  const otelcolConfigInvalidUndeclaredExtension = readFileSync(join(assetFolderPath, 'config-undeclared-extension.yaml')).toString();
+
+  const otelcolConfigInvalidUndeclaredReceiver = readFileSync(join(assetFolderPath, 'config-undeclared-receiver.yaml')).toString();
+
+  const defaultTimeout = 10_000; // 10 seconds
+
+  describe(`POST ${validationReleasePath}`, () => {
+
+    axios.defaults.baseURL = validationUrl;
+
+    test('rejects unauthenticated requests', async () => {
+      await expect(axios.post(validationUrl, otelcolConfigValid)).rejects.toThrow();
+    }, defaultTimeout);
+
+    test('accepts valid configuration', async () => {
+      await expect(axios.post(validationUrl, otelcolConfigValid, {
+        headers: {
+          'Content-Type': 'application/yaml',
+          'X-Api-Key': apiKey,
+        },
+      })).resolves.toMatchObject({
+        status: 200,
+        data: {
+          message: 'Configuration is valid',
+        },
+      });
+    }, defaultTimeout);
+
+    test('rejects empty configuration', async () => {
+      await expect(axios.post(validationUrl, '', {
+        headers: {
+          'Content-Type': 'application/yaml',
+          'X-Api-Key': apiKey,
+        },
+      })).resolves.toMatchObject({
+        status: 200,
+        data: {
+          message: 'The provided configuration is invalid',
+          error: 'the provided configuration is empty',
+        },
+      });
+    }, defaultTimeout);
+
+    test('rejects configuration without declared receivers', async () => {
+      await expect(axios.post(validationUrl, otelcolConfigInvalidNoReceivers, {
+        headers: {
+          'Content-Type': 'application/yaml',
+          'X-Api-Key': apiKey,
+        },
+      })).resolves.toMatchObject({
+        status: 200,
+        data: {
+          message: 'The provided configuration is invalid',
+          error: 'no receiver configuration specified in config',
+        },
+      });
+    }, defaultTimeout);
+
+    test('rejects configuration with undeclared receiver', async () => {
+      await expect(axios.post(validationUrl, otelcolConfigInvalidUndeclaredReceiver, {
+        headers: {
+          'Content-Type': 'application/yaml',
+          'X-Api-Key': apiKey,
+        },
+      })).resolves.toMatchObject({
+        status: 200,
+        data: {
+          message: 'The provided configuration is invalid',
+          error: 'service::pipelines::traces: references receiver "jaeger" which is not configured',
+        },
+      });
+    }, defaultTimeout);
+
+    test('rejects configuration with undeclared extension', async () => {
+      await expect(axios.post(validationUrl, otelcolConfigInvalidUndeclaredExtension, {
+        headers: {
+          'Content-Type': 'application/yaml',
+          'X-Api-Key': apiKey,
+        },
+      })).resolves.toMatchObject({
+        status: 200,
+        data: {
+          message: 'The provided configuration is invalid',
+          error: 'service::extensions: references extension "health_check" which is not configured',
+        },
+      });
+    }, defaultTimeout);
 
   });
 
