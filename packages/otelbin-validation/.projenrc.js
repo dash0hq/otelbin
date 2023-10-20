197ce1671d657903b4645978d7eab0de94d67e1c
Michele Mancioppi <michele.mancioppi@dash0.com>
2023-10-20 11:47:26 +0200

    chore: skip eslint for otelbin-validation image

--- packages/otelbin-validation/.projenrc.js
+++ packages/otelbin-validation/.projenrc.js
@@ -7,5 +7,15 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   devDeps: ['@jest/globals', '@types/aws-lambda', 'axios', 'esbuild'],
   github: false, // Skip GitHub integration, as this CDK app is not in the repo's root
   packageManager: 'npm',
+  eslintOptions: {
+    ignorePatterns: [
+      '*.js',
+      '*.d.ts',
+      'node_modules/',
+      '*.generated.ts',
+      'coverage',
+      './src/images/otelcol-validator/'
+    ]
+  }
 });
 project.synth();
