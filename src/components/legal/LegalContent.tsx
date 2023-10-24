67b4a9c977567c3c9c6e1869b56d0d3b5ba8fd1d
Ben Blackmore <ben.blackmore@dash0.com>
2023-10-24 12:54:59 +0200

    fix: font color in legal popover

--- src/components/legal/LegalContent.tsx
+++ src/components/legal/LegalContent.tsx
@@ -6,10 +6,10 @@ import { legalDocuments } from "~/components/legal/legalDocuments";
 export function LegalContent() {
 	return (
 		<div>
-			<ul className="mx-4 my-3 text-sm font-normal text-default">
+			<ul className="mx-4 my-3 text-sm font-normal text-subtl">
 				{legalDocuments.map((doc) => (
 					<li key={doc.label}>
-						<a href={doc.url} target="_blank" className="hover:text-primary-hover">
+						<a href={doc.url} target="_blank" className="hover:text-default">
 							{doc.label}
 						</a>
 					</li>
