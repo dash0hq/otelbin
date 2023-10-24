548500af237bf58b9b0ce95dbc9b4edfe1acb92d
Ben Blackmore <ben.blackmore@dash0.com>
2023-10-24 13:34:19 +0200

    feat: change error message when distro retrieval fails

--- src/app/validation/supported-distributions/route.ts
+++ src/app/validation/supported-distributions/route.ts
@@ -6,23 +6,36 @@ import { NextResponse } from "next/server";
 import { assertValue } from "~/lib/env";
 
 export async function GET(): Promise<NextResponse> {
-	const response = await retryFetch(fetch)(
-		`${assertValue(
-			process.env.COLLECTOR_CONFIGURATION_VALIDATION_URL,
-			"COLLECTOR_CONFIGURATION_VALIDATION_URL env var is not configured"
-		)}/validation/supported-distributions`,
-		{
-			headers: {
-				"X-API-KEY": assertValue(
-					process.env.COLLECTOR_CONFIGURATION_VALIDATION_API_KEY,
-					"COLLECTOR_CONFIGURATION_VALIDATION_API_KEY env var is not configured"
-				),
+	let response: Response;
+
+	try {
+		response = await retryFetch(fetch)(
+			`${assertValue(
+				process.env.COLLECTOR_CONFIGURATION_VALIDATION_URL,
+				"COLLECTOR_CONFIGURATION_VALIDATION_URL env var is not configured"
+			)}/validation/supported-distributions`,
+			{
+				headers: {
+					"X-API-KEY": assertValue(
+						process.env.COLLECTOR_CONFIGURATION_VALIDATION_API_KEY,
+						"COLLECTOR_CONFIGURATION_VALIDATION_API_KEY env var is not configured"
+					),
+				},
+				retries: 3,
+				retryDelay: 1000,
+				retryOn: [500, 503],
+			}
+		);
+	} catch (e) {
+		console.error("Failed to retrieve supported distros", e);
+
+		return NextResponse.json(
+			{
+				error: "Failed to retrieve supported distributions",
 			},
-			retries: 3,
-			retryDelay: 1000,
-			retryOn: [500, 503],
-		}
-	);
+			{ status: 500 }
+		);
+	}
 
 	if (!response.ok) {
 		const body = await response.text();
