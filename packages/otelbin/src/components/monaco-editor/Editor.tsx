af26dcbe8bfc0c258c937f0867fc47f0dda0ae5e
samueljrz <samuelevangelista.ti@gmail.com>
2023-11-15 01:28:16 -0300

    change the ReactFlowProviderf scope to pass context to share component

--- packages/otelbin/src/components/monaco-editor/Editor.tsx
+++ packages/otelbin/src/components/monaco-editor/Editor.tsx
@@ -148,76 +148,76 @@ export default function Editor({ locked, setLocked }: { locked: boolean; setLock
 		<>
 			<WelcomeModal open={openDialog} setOpen={setOpenDialog} />
 			<div className="flex h-full max-h-screen min-h-screen flex-col">
-				<AppHeader />
-				<div className="flex h-full w-full shrink grow">
-					<div
-						className={`relative flex shrink-0 flex-col`}
-						style={{
-							width: calculateViewWidth(viewMode, width),
-						}}
-					>
-						<EditorTopBar config={currentConfig} font={firaCode} />
-						<div className={`h-full w-full shrink grow ${firaCode.className}`}>
-							{clerk.loaded && (
+				<ReactFlowProvider>
+					<AppHeader />
+					<div className="flex h-full w-full shrink grow">
+						<div
+							className={`relative flex shrink-0 flex-col`}
+							style={{
+								width: calculateViewWidth(viewMode, width),
+							}}
+						>
+							<EditorTopBar config={currentConfig} font={firaCode} />
+							<div className={`h-full w-full shrink grow ${firaCode.className}`}>
+								{clerk.loaded && (
+									<AutoSizer>
+										{({ width, height }) => (
+											<MonacoEditor
+												defaultValue={config}
+												value={config}
+												onMount={editorDidMount}
+												width={width}
+												height={height}
+												defaultLanguage="yaml"
+												theme="OTelBin"
+												options={{
+													quickSuggestions: { other: true, strings: true },
+													automaticLayout: true,
+													minimap: { enabled: false },
+													scrollbar: { verticalScrollbarSize: 8, horizontal: "hidden" },
+													padding: { top: 5 },
+													fontSize: 13,
+													fontWeight: "400",
+													fontFamily: firaCode.style.fontFamily,
+												}}
+												onChange={handleEditorChange}
+											/>
+										)}
+									</AutoSizer>
+								)}
+							</div>
+							{viewMode !== "pipeline" && <ValidationErrorConsole errors={totalValidationErrors} font={firaCode} />}
+							{viewMode == "both" && <ResizeBar onWidthChange={onWidthChange} />}
+						</div>
+						<div className="z-0 min-h-full w-full shrink grow relative">
 								<AutoSizer>
 									{({ width, height }) => (
-										<MonacoEditor
-											defaultValue={config}
-											value={config}
-											onMount={editorDidMount}
-											width={width}
-											height={height}
-											defaultLanguage="yaml"
-											theme="OTelBin"
-											options={{
-												quickSuggestions: { other: true, strings: true },
-												automaticLayout: true,
-												minimap: { enabled: false },
-												scrollbar: { verticalScrollbarSize: 8, horizontal: "hidden" },
-												padding: { top: 5 },
-												fontSize: 13,
-												fontWeight: "400",
-												fontFamily: firaCode.style.fontFamily,
-											}}
-											onChange={handleEditorChange}
-										/>
+										<div style={{ width: `${width}px`, height: `${height}px` }}>
+											<Flow
+												value={(isValidConfig && currentConfig) || "{}"}
+												openDialog={setOpenDialog}
+												locked={locked}
+												setLocked={setLocked}
+												editorRef={editorRef}
+											/>
+										</div>
 									)}
 								</AutoSizer>
+
+							{viewMode === "pipeline" && (
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<IconButton onClick={() => setViewMode("both")} size={"xs"} className="absolute top-4 left-4 z-1">
+											<PanelLeftOpen />
+										</IconButton>
+									</TooltipTrigger>
+									<TooltipContent>Show editor</TooltipContent>
+								</Tooltip>
 							)}
 						</div>
-						{viewMode !== "pipeline" && <ValidationErrorConsole errors={totalValidationErrors} font={firaCode} />}
-						{viewMode == "both" && <ResizeBar onWidthChange={onWidthChange} />}
 					</div>
-					<div className="z-0 min-h-full w-full shrink grow relative">
-						<ReactFlowProvider>
-							<AutoSizer>
-								{({ width, height }) => (
-									<div style={{ width: `${width}px`, height: `${height}px` }}>
-										<Flow
-											value={(isValidConfig && currentConfig) || "{}"}
-											openDialog={setOpenDialog}
-											locked={locked}
-											setLocked={setLocked}
-											editorRef={editorRef}
-										/>
-									</div>
-								)}
-							</AutoSizer>
-						</ReactFlowProvider>
-
-						{viewMode === "pipeline" && (
-							<Tooltip>
-								<TooltipTrigger asChild>
-									<IconButton onClick={() => setViewMode("both")} size={"xs"} className="absolute top-4 left-4 z-1">
-										<PanelLeftOpen />
-									</IconButton>
-								</TooltipTrigger>
-								<TooltipContent>Show editor</TooltipContent>
-							</Tooltip>
-						)}
-					</div>
-				</div>
-				<AppFooter />
+					<AppFooter />
+				</ReactFlowProvider>
 			</div>
 		</>
 	);
