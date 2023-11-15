418b12aeb5961bb466f9e9939796b8648339476d
samueljrz <samuelevangelista.ti@gmail.com>
2023-11-15 01:32:50 -0300

    add: new component DownloadButton

---
+++ packages/otelbin/src/components/share/DownloadButton.tsx
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: 2023 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+import { useReactFlow, getRectOfNodes, getTransformForBounds } from 'reactflow';
+import { ImageDown } from "lucide-react";
+import { Button } from "~/components/button";
+import { toPng } from 'html-to-image';
+
+const downloadImage = (dataUrl: string) => {
+  const a = document.createElement('a');
+
+  a.setAttribute('download', 'collector-image.png');
+  a.setAttribute('href', dataUrl);
+  a.click();
+};
+
+const calculateImageDimensions = (nodeCount: number) => {
+  const baseWidth = 1024;
+  const baseHeight = 768;
+  const widthPerNode = 100; 
+
+  const calculatedWidth = Math.max(baseWidth, widthPerNode * nodeCount);
+
+  return {
+    width: calculatedWidth,
+    height: baseHeight,
+  };
+}
+
+export function DownloadButton() {
+  const { getNodes } = useReactFlow();
+
+  const onClick = () => {
+    const nodes = getNodes()
+    const nodesBounds = getRectOfNodes(nodes)
+    const { width, height } = calculateImageDimensions(nodes.length);
+    const transform = getTransformForBounds(nodesBounds, width, height, 0.5, 2)
+
+    toPng(document.querySelector('.react-flow__viewport') as HTMLElement, {
+      backgroundColor: '#333333',
+      pixelRatio: 2,
+      width: width,
+      height: height,
+      style: {
+        width: width.toString() ,
+        height: height.toString(),
+        transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
+      },
+    }).then(downloadImage);
+  };
+
+  return (
+    <Button asChild size="xs">
+      <a style={{cursor: 'pointer'}} onClick={onClick}>
+        <ImageDown className="mr-1" />
+        Download Image
+      </a>
+    </Button>
+  );
+}
