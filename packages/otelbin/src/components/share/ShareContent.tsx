9f01f422a7491fe77023805020f46a2452c43d7a
Ben Blackmore <ben.blackmore@dash0.com>
2023-11-23 19:50:29 +0100

    feat: show shield copy in share menu
    Fixes https://github.com/dash0hq/otelbin/issues/165

--- packages/otelbin/src/components/share/ShareContent.tsx
+++ packages/otelbin/src/components/share/ShareContent.tsx
@@ -1,39 +1,78 @@
 // SPDX-FileCopyrightText: 2023 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, useAuth } from "@clerk/nextjs";
 import { Button } from "~/components/button";
-import { ArrowDownToLine, LogIn } from "lucide-react";
+import { ArrowDownToLine, Copy, LogIn } from "lucide-react";
 import { useUrlState } from "~/lib/urlState/client/useUrlState";
 import { editorBinding } from "~/components/monaco-editor/editorBinding";
 import { UrlCopy } from "~/components/share/UrlCopy";
-import { SignedInUrlSharing } from "~/components/share/SignedInUrlSharing";
 import { DownloadImageButton } from "~/components/share/DownloadImageButton";
 import { track } from "@vercel/analytics";
+import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/tooltip";
+import { IconButton } from "~/components/icon-button";
+import { useToast } from "~/components/use-toast";
+import useSWR from "swr";
+import { fetcher } from "~/lib/fetcher";
+import useSWRImmutable from "swr/immutable";
 
 export function ShareContent() {
+	const { toast } = useToast();
 	const [{ config }, getLink] = useUrlState([editorBinding]);
-	const fullURL = window.location.origin + getLink({});
+	let fullURL = window.location.origin + getLink({});
+	const { isSignedIn } = useAuth();
+
+	const { data } = useSWRImmutable<{ shortLink: string }>(
+		isSignedIn
+			? {
+					url: `/s/new`,
+					method: "POST",
+					body: fullURL,
+			  }
+			: undefined,
+		fetcher
+	);
+
+	fullURL = data?.shortLink ?? fullURL;
 
 	return (
 		<div>
-			<p className="weight mx-4 my-3 text-sm font-normal text-default">Use the URL to share the config with others.</p>
-
-			<SignedIn>
-				<SignedInUrlSharing fullURL={fullURL} />
-			</SignedIn>
-			<SignedOut>
-				<UrlCopy url={fullURL} />
-
+			<p className="weight mx-4 mt-3 mb-2 text-sm font-normal text-default">Share the config with others.</p>
+			<UrlCopy url={fullURL} />
+			{!isSignedIn && (
 				<SignInButton mode="modal" afterSignInUrl="/restore">
 					<Button size="xs" variant="cta" className="mx-4 mt-3">
 						<LogIn className="mr-1" />
 						Sign in to create short URL
 					</Button>
 				</SignInButton>
-			</SignedOut>
+			)}
 
-			<div className="mt-3 border-t-1 border-subtle px-4 py-3">
+			{isSignedIn && (
+				<div className="mt-3 border-t-1 border-subtle px-4 pt-3">
+					<p className="weight mb-2 text-sm font-normal text-default">Use a badge within your repository or docs.</p>
+
+					<div className="flex gap-2 items-center">
+						<img
+							src="/shields/collector-config"
+							alt="OpenTelemetry collector configuration on OTelBin"
+							width={167}
+							height={20}
+							className="max-h-[20px] grow-0"
+						/>
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<IconButton size="xs" onClick={copyToClipboard}>
+									<Copy />
+								</IconButton>
+							</TooltipTrigger>
+							<TooltipContent>Copy badge markdown to clipboard</TooltipContent>
+						</Tooltip>
+					</div>
+				</div>
+			)}
+
+			<div className="mt-4 border-t-1 border-subtle px-4 py-3">
 				<Button asChild size="xs" className="mr-2">
 					<a
 						href={`data:text/plain;base64,${btoa(config)}`}
@@ -48,4 +87,24 @@ export function ShareContent() {
 			</div>
 		</div>
 	);
+
+	function copyToClipboard() {
+		const text = `[![OpenTelemetry collector configuration on OTelBin](${
+			window.location.origin + "/shields/collector-config"
+		})](${fullURL})`;
+		navigator.clipboard
+			.writeText(text)
+			.then(() => {
+				toast({
+					description: "Copied to clipboard.",
+				});
+			})
+			.catch((e) => {
+				console.error("Failed to copy to clipboard", e);
+				toast({
+					description: "Failed to copy to clipboard",
+				});
+			});
+		track("Shared Link");
+	}
 }
