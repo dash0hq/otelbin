c828b12bcbd15928daf6020c129c35ec967cca29
Roshan Ghojoghi <roshan.ghojoghi@dash0.com>
2023-11-28 15:28:42 +0300

    fix: refactor draw openGraph image

--- packages/otelbin/src/app/og/ParentsNode.tsx
+++ packages/otelbin/src/app/og/ParentsNode.tsx
@@ -1,17 +1,12 @@
 // SPDX-FileCopyrightText: 2023 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-// import ParentNodeTag from "./ParentNodeTag";
-import { calcNodes } from "../../components/react-flow/useClientNodes";
 import React from "react";
+import { type Node } from "reactflow";
 import ParentNodeTag from "./ParentNodeTag";
 import { BarChart4, ListTree, Workflow } from "lucide-react";
-
-interface IData {
-	label: string;
-	height: number;
-	position: { x: number; y: number };
-}
+import ArrowRight from "./svg/move-right.svg";
+import { ReceiversNode, ProcessorsNode, ExportersNode } from "./NodeTypes";
 
 export const parentNodesConfig = [
 	{
@@ -48,9 +43,8 @@ export const parentNodesConfig = [
 	},
 ];
 
-const ParentsNode = ({ data, jsonData, children }: { data: IData; jsonData: any; children: React.ReactNode }) => {
-	const nodes = calcNodes(jsonData);
-	const childNodes = nodes?.filter((node) => node.parentNode === data.label);
+const ParentsNode = ({ nodeData, nodes }: { nodeData: Node; nodes?: Node[] }) => {
+	const childNodes = nodes?.filter((node) => node.parentNode === nodeData.data.label);
 	const processorsNodesCount = childNodes?.filter((node) => node.type === "processorsNode").length ?? 0;
 	const nodesWidth = 120;
 	const sumOfExporterAndReceiver = 240;
@@ -59,10 +53,56 @@ const ParentsNode = ({ data, jsonData, children }: { data: IData; jsonData: any;
 	const totalEdgeWidth = edgesWidth * (processorsNodesCount + 1);
 	const maxWidth = totalNodesWidth + (processorsNodesCount + 2) * 20 + totalEdgeWidth;
 
+	const receivers = nodes
+		?.filter((node) => node.type === "receiversNode")
+		.filter((receiver) => receiver.parentNode === nodeData.data.label);
+	const exporters = nodes
+		?.filter((node) => node.type === "exportersNode")
+		.filter((exporter) => exporter.parentNode === nodeData.data.label);
+	const processors = nodes
+		?.filter((node) => node.type === "processorsNode")
+		.filter((processor) => processor.parentNode === nodeData.data.label);
+
+	const nodeHeight = 80;
+	const nodeTotalMargin = 40;
+
+	function calcSVGHeight(nodes?: Node[]) {
+		const nodesCount = nodes?.length ?? 0;
+		return nodesCount * (nodeHeight + nodeTotalMargin) - nodeTotalMargin;
+	}
+
+	function calcSVGPath(side: string, nodes?: Node[]) {
+		const nodesCount = nodes?.length ?? 0;
+		const height = nodesCount * (nodeHeight + nodeTotalMargin) - 80;
+
+		return (
+			<svg style={{ marginBottom: "30px" }} width="80" height={calcSVGHeight(nodes)} xmlns="http://www.w3.org/2000/svg">
+				{Array.isArray(nodes) &&
+					nodes?.length > 0 &&
+					nodes.map((_, idx) => (
+						<path
+							key={idx}
+							d={
+								side === "left"
+									? `M10 ${(idx + 1) * (height / nodesCount) - 60} 
+				    C 20,${(idx + 1) * (height / nodesCount) - 60},35,${height / 2 - 20}  
+						 50 ${height / 2 - 20}`
+									: `M10 ${height / 2 - 20}
+						C 20,${height / 2 - 20},35,${(idx + 1) * (height / nodesCount) - 60}  
+							 50 ${(idx + 1) * (height / nodesCount) - 60}`
+							}
+							stroke="#FFFFFF"
+							fill="transparent"
+						/>
+					))}
+			</svg>
+		);
+	}
+
 	return (
 		<>
 			{parentNodesConfig
-				.filter((config) => data.label.match(config.typeRegex))
+				.filter((config) => nodeData.data.label.match(config.typeRegex))
 				.map((node, idx) => {
 					return (
 						<div
@@ -74,14 +114,35 @@ const ParentsNode = ({ data, jsonData, children }: { data: IData; jsonData: any;
 								position: "relative",
 								backgroundColor: node.backgroundColor,
 								border: node.borderColor,
-								height: data.height,
+								height: nodeData.data.height,
 								width: maxWidth,
 							}}
 							tw="rounded-[4px] text-[10px] text-black my-3"
 						>
-							<ParentNodeTag tag={data.label} />
+							<ParentNodeTag tag={nodeData.data.label} />
 
-							<div style={{ display: "flex", justifyContent: "center" }}>{children}</div>
+							<div style={{ display: "flex", justifyContent: "center" }}>
+								<div tw="flex items-center">
+									<div tw="flex flex-col justify-center">
+										{receivers?.map((receiver, idx) => <ReceiversNode key={idx} data={receiver.data} />)}
+									</div>
+									{receivers?.length === 1 ? <ArrowRight /> : calcSVGPath("left", receivers)}
+								</div>
+								<div tw="flex items-center">
+									<div tw="flex justify-center items-center">
+										{processors?.map((processor, idx) => (
+											<div key={idx} tw="flex justify-center items-center">
+												{idx > 0 ? <ArrowRight /> : <></>}
+												<ProcessorsNode key={idx} data={processor.data} />
+											</div>
+										))}
+									</div>
+									{exporters?.length === 1 ? <ArrowRight /> : calcSVGPath("right", exporters)}
+									<div tw="flex flex-col justify-center">
+										{exporters?.map((exporter, idx) => <ExportersNode key={idx} data={exporter.data} />)}
+									</div>
+								</div>
+							</div>
 						</div>
 					);
 				})}
--- packages/otelbin/src/app/og/route.tsx
+++ packages/otelbin/src/app/og/route.tsx
@@ -5,9 +5,9 @@ import { ImageResponse } from "next/server";
 import { calcNodes } from "~/components/react-flow/useClientNodes";
 import { type Node } from "reactflow";
 import ParentsNode from "./ParentsNode";
-import ArrowRight from "./svg/move-right.svg";
-import { ReceiversNode, ProcessorsNode, ExportersNode } from "./NodeTypes";
+
 export const runtime = "edge";
+
 const jsonData = {
 	connectors: {
 		otlp: {
@@ -74,32 +74,29 @@ const jsonData = {
 	service: {
 		pipelines: {
 			traces: {
-				receivers: ["otlp", "zipkin"],
+				receivers: ["otlp"],
 				processors: ["batch"],
-				exporters: ["otlp", "zipkin", "otlp2", "otlp8", "otlp9", "otlp10"],
+				exporters: ["otlp"],
 			},
 			metrics: {
-				receivers: ["otlp2", "otlp3", "otlp4", "otlp5"],
-				processors: ["batch", "memory_limiter", "queued_retry", "metricstransform"],
+				receivers: ["otlp2"],
+				processors: ["batch"],
 				exporters: ["otlp"],
 			},
 			logs: {
-				receivers: ["otlp3", "otlp4", "otlp5", "otlp6", "otlp7", "otlp8", "otlp9", "otlp10"],
-				processors: ["batch", "memory_limiter", "queued_retry", "resource", "tail_sampling", "metricstransform"],
-				exporters: ["otlp", "zipkin"],
+				receivers: ["otlp3"],
+				processors: ["batch"],
+				exporters: ["otlp"],
 			},
 		},
 	},
 };
+
 export function GET() {
 	const edgeWidth = 80;
-	const nodeHeight = 80;
-	const nodeTotalMargin = 40;
 	const initNodes = calcNodes(jsonData, true);
 	const parentNodes = initNodes?.filter((node) => node.type === "parentNodeType");
-	const receivers = initNodes?.filter((node) => node.type === "receiversNode");
 	const processors = initNodes?.filter((node) => node.type === "processorsNode");
-	const exporters = initNodes?.filter((node) => node.type === "exportersNode");
 
 	function calcScale(nodes?: Node[]) {
 		const targetHeight = 630;
@@ -110,36 +107,6 @@ export function GET() {
 		const totalWidth = totalHorizontalNodesCount * 140 + (totalHorizontalNodesCount - 1) * edgeWidth;
 		const scale = Math.min(targetHeight / totalHeight, targetWidth / totalWidth);
 		return scale.toString();
-	}
-
-	function calcSVGHeight(nodes?: Node[]) {
-		const nodesCount = nodes?.length ?? 0;
-		return nodesCount * (nodeHeight + nodeTotalMargin) - nodeTotalMargin;
-	}
-
-	function calcSVGPath(side: string, nodes?: Node[]) {
-		const nodesCount = nodes?.length ?? 0;
-		const height = nodesCount * (nodeHeight + nodeTotalMargin) - 80;
-		return (
-			Array.isArray(nodes) &&
-			nodes?.length > 0 &&
-			nodes.map((_, idx) => (
-				<path
-					key={idx}
-					d={
-						side === "left"
-							? `M10 ${(idx + 1) * (height / nodesCount) - 60} 
-				    C 20,${(idx + 1) * (height / nodesCount) - 60},35,${height / 2 - 20}  
-						 50 ${height / 2 - 20}`
-							: `M10 ${height / 2 - 20}
-						C 20,${height / 2 - 20},35,${(idx + 1) * (height / nodesCount) - 60}  
-							 50 ${(idx + 1) * (height / nodesCount) - 60}`
-					}
-					stroke="#FFFFFF"
-					fill="transparent"
-				/>
-			))
-		);
 	}
 
 	return new ImageResponse(
@@ -165,58 +132,7 @@ export function GET() {
 					}}
 					tw="bg-[#151721]"
 				>
-					{parentNodes?.map((node, idx) => (
-						<ParentsNode key={idx} data={node.data} jsonData={jsonData}>
-							<div tw="flex items-center">
-								<div tw="flex flex-col justify-center">
-									{receivers
-										?.filter((receiver) => receiver.parentNode === node.data.label)
-										.map((receiver, idx) => <ReceiversNode key={idx} data={receiver.data} />)}
-								</div>
-								{receivers?.filter((receiver) => receiver.parentNode === node.data.label).length === 1 ? (
-									<ArrowRight />
-								) : (
-									<svg
-										style={{ marginBottom: "30px" }}
-										width="80"
-										height={calcSVGHeight(receivers?.filter((receiver) => receiver.parentNode === node.data.label))}
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										{calcSVGPath("left", receivers?.filter((receiver) => receiver.parentNode === node.data.label))}
-									</svg>
-								)}
-							</div>
-							<div tw="flex items-center">
-								<div tw="flex justify-center items-center">
-									{processors
-										?.filter((processor) => processor.parentNode === node.data.label)
-										.map((processor, idx) => (
-											<div key={idx} tw="flex justify-center items-center">
-												{idx > 0 ? <ArrowRight /> : <></>}
-												<ProcessorsNode key={idx} data={processor.data} />
-											</div>
-										))}
-								</div>
-								{exporters?.filter((exporter) => exporter.parentNode === node.data.label).length === 1 ? (
-									<ArrowRight />
-								) : (
-									<svg
-										style={{ marginBottom: "30px" }}
-										width="80"
-										height={calcSVGHeight(exporters?.filter((exporter) => exporter.parentNode === node.data.label))}
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										{calcSVGPath("right", exporters?.filter((exporter) => exporter.parentNode === node.data.label))}
-									</svg>
-								)}
-								<div tw="flex flex-col justify-center">
-									{exporters
-										?.filter((exporter) => exporter.parentNode === node.data.label)
-										.map((exporter, idx) => <ExportersNode key={idx} data={exporter.data} />)}
-								</div>
-							</div>
-						</ParentsNode>
-					))}
+					{parentNodes?.map((parentNode, idx) => <ParentsNode key={idx} nodeData={parentNode} nodes={initNodes} />)}
 				</div>
 			</div>
 		),
