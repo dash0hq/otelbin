22a66ce4fd19c356ecefe150f42f942b8c0153fa
Roshan Ghojoghi <roshan.ghojoghi@dash0.com>
2023-12-07 18:36:58 +0300

    fix: add slack bots to isBotRequest

--- packages/otelbin/src/lib/utils.ts
+++ packages/otelbin/src/lib/utils.ts
@@ -2,22 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { type ClassValue, clsx } from "clsx";
+import type { NextRequest } from "next/server";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-
-export const isBotRequest = (req: Request): boolean => {
-
-	const searchParams = new URL(req.url).searchParams;
-	if (searchParams.get("bot")) return true;
+export function isBotRequest(req: NextRequest): boolean {
+	if (req.nextUrl.searchParams.get("bot")) {
+		return true
+	}
 	const userAgent = req.headers.get("User-Agent");
 	if (userAgent) {
-		return /bot|teoma|yandex|baidu|WhatsApp|google|ChatGPT|bing|msn|duckduckbot|facebookexternalhit|slurp|MetaInspector/i.test(
+		return /bot|teoma|yandex|baidu|WhatsApp|google|ChatGPT|bing|msn|duckduckbot|facebookexternalhit|TelegramBot|slurp|MetaInspector|Slackbot-LinkExpanding|Slack-ImgProxy|Slackbot/i.test(
 			userAgent,
 		);
 	}
 	return false;
-};
+}
