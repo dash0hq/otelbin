b9c7d0a2ef55f9d04b042ce6632744f57fbafab9
haoqixu <hq.xu0o0@gmail.com>
2023-12-14 00:47:29 +0800

    chore: remove unused import

--- packages/otelbin/src/components/react-flow/CyclicErrorEdge.tsx
+++ packages/otelbin/src/components/react-flow/CyclicErrorEdge.tsx
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState } from "react";
 import { type EdgeProps, BaseEdge, getSmoothStepPath, EdgeLabelRenderer } from "reactflow";
 
 interface CyclicErrorEdgeProps extends EdgeProps {
