464edc888258b90776878d650f6dec479baf5a7f
Roshan Ghojoghi <roshan.ghojoghi@dash0.com>
2023-12-14 22:31:43 +0300

    fix: deduplicate utils.test.ts

--- packages/otelbin/src/lib/utils.test.ts
+++ packages/otelbin/src/lib/utils.test.ts
@@ -6,42 +6,44 @@ import { isBotRequest } from "./utils";
 import { NextRequest } from "next/server";
 
 describe("isBotRequest", () => {
-	it("should return true for user-agent bot requests", () => {
-		const mockRequest = new Request("https://www.whatever.com/admin/check-it-out");
+	const createMockRequest = (url: string, userAgent?: string) => {
+		const mockRequest = new Request(url);
 		const mockNextRequest = new NextRequest(mockRequest, {});
-		mockNextRequest.headers.set("User-Agent", "Slackbot 1.0 (+https://api.slack.com/robots)");
+		if (userAgent) {
+			mockNextRequest.headers.set("User-Agent", userAgent);
+		}
+		return mockNextRequest;
+	};
 
+	it("should return true for user-agent bot requests", () => {
+		const mockNextRequest = createMockRequest(
+			"https://www.whatever.com/admin/check-it-out",
+			"Slackbot 1.0 (+https://api.slack.com/robots)"
+		);
 		const result = isBotRequest(mockNextRequest);
 
 		expect(result).toBe(true);
 	});
 
 	it("should return true for url param ?bot=true requests", () => {
-		const mockRequest = new Request("https://www.whatever.com/admin/check-it-out?bot=true");
-		const mockNextRequest = new NextRequest(mockRequest, {});
-
+		const mockNextRequest = createMockRequest("https://www.whatever.com/admin/check-it-out?bot=true");
 		const result = isBotRequest(mockNextRequest);
 
 		expect(result).toBe(true);
 	});
 
 	it("should return true for url param ?bot=false requests", () => {
-		const mockRequest = new Request("https://www.whatever.com/admin/check-it-out?bot=false");
-		const mockNextRequest = new NextRequest(mockRequest, {});
-
+		const mockNextRequest = createMockRequest("https://www.whatever.com/admin/check-it-out?bot=false");
 		const result = isBotRequest(mockNextRequest);
 
 		expect(result).toBe(true);
 	});
 
 	it("should return false for non-bot requests", () => {
-		const mockRequest = new Request("https://www.whatever.com/admin/check-it-out");
-		const mockNextRequest = new NextRequest(mockRequest, {});
-		mockNextRequest.headers.set(
-			"User-Agent",
+		const mockNextRequest = createMockRequest(
+			"https://www.whatever.com/admin/check-it-out",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.3029.110 Safari/537"
 		);
-
 		const result = isBotRequest(mockNextRequest);
 
 		expect(result).toBe(false);
