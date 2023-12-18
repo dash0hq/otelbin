2244ab4bd0a3aa20f953c435d89d4be64f137113
Marcel Birkner <marcel.birkner@dash0.com>
2023-12-18 11:15:11 +0100

    disable typescript-eslint/no-explicit-any

--- packages/otelbin/.eslintrc.cjs
+++ packages/otelbin/.eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
 	plugins: ["@typescript-eslint", "header"],
 	extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
 	rules: {
+		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/consistent-type-imports": [
 			"warn",
 			{
