606a28e12dca24a448acfde60c7f9e0631b4479c
Michele Mancioppi <michele.mancioppi@dash0.com>
2024-01-29 15:40:30 +0100

    replace underscore with hyphen in test env name

--- .github/workflows/scripts/test_env_name.sh
+++ .github/workflows/scripts/test_env_name.sh
@@ -4,7 +4,7 @@
 # This logic ensures test envs have max length 18. Names of length 13 to 17 will become of length 18 to ensure
 # no accidental clashes.
 if [[ ${#REF_NAME} -gt 12 ]]; then
-    REF_NAME="${REF_NAME:0:12}-$(echo "${REF_NAME:12}" | md5sum | awk '{print $1}' | cut -c 1-5 | tr -d '\n')"
+    REF_NAME="${REF_NAME:0:12}-$(echo "${REF_NAME:12}" | md5sum | awk '{print $1}' | tr '_' '-' | cut -c 1-5 | tr -d '\n')"
 fi
 
 echo "${REF_NAME}"
